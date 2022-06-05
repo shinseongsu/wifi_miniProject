9eb3226a2f1990d29303815339515dac7ebd5630
Shin seongsu <tlstjdtn321@naver.com>
2022-06-05 23:53:09 +0900

    feat: clear 추가

--- src/main/java/com/example/miniproject/config/HibernateManager.java
+++ src/main/java/com/example/miniproject/config/HibernateManager.java
@@ -2,7 +2,6 @@ package com.example.miniproject.config;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class HibernateManager {
@@ -27,10 +26,12 @@ public class HibernateManager {
 
     public static void txCommit() {
         getInstance().getTransaction().commit();
+        getInstance().clear();
     }
 
     public static void txRollback() {
         getInstance().getTransaction().rollback();
+        getInstance().clear();
     }
 
 }
